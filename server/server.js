41f221fa566b14944ca6c80a30604576e34b5f4b
Will <wilito1210@gmail.com>
2022-05-19 13:54:27 -0400

    client side and queries

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 // import ApolloServer 
 const {ApolloServer} = require('apollo-server-express');
 const {authMiddleware} = require('./utils/auth')
+const path = require('path')
 //import our typeDefs and resolvers 
 const {typeDefs, resolvers} = require('./schemas')
 const db = require('./config/connection');
@@ -24,6 +25,13 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // integrate our apollo server with the express app as a middleware
   server.applyMiddleware({app});
 
+  if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,'../client/build')))
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'../client/build/index.html'))
+  })
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
